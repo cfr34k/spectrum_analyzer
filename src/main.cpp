5c9c49a713e2e1ec8d50845067d681b900c4b100
Thomas Kolb <thomas@tkolb.de>
2015-07-08 20:49:32 +0200

    Summarize spectrum data in Graph
    Displays noise floor, average and peak power.

--- src/main.cpp
+++ src/main.cpp
@@ -6,33 +6,88 @@
 #include "Receiver.h"
 #include "version.h"
 
-/*
-void readFFTData(size_t N, FFT::fft_complex *data)
+void resultToScreenData(const SpectrumAccumulator::AmplitudeVector &spectrum,
+                        size_t nbins,
+                        SpectrumAccumulator::AmplitudeVector *noiseFloor,
+                        SpectrumAccumulator::AmplitudeVector *avgPower,
+                        SpectrumAccumulator::AmplitudeVector *peakPower)
 {
-	for(size_t i = 0; i < N; i++) {
-		uint8_t val;
+	size_t curBin = 0;
+	float startSample = 0;
+	float samplesPerBin = static_cast<float>(spectrum.size()) / nbins;
 
-		std::cin.read(reinterpret_cast<char*>(&val), sizeof(val));
-		data[i].real(val / 128.);
+	noiseFloor->resize(nbins);
+	avgPower->resize(nbins);
+	peakPower->resize(nbins);
 
-		std::cin.read(reinterpret_cast<char*>(&val), sizeof(val));
-		data[i].imag(val / 128.);
+	while(startSample < (spectrum.size() - samplesPerBin)) {
+		float endSample = startSample + samplesPerBin;
+
+		float min = 1e9, avg = 0, max = 0;
+		size_t n = 0;
+		for(size_t s = startSample; s < endSample; s++) {
+			avg += spectrum[s];
+			n++;
+
+			if(spectrum[s] > max) {
+				max = spectrum[s];
+			}
+
+			if(spectrum[s] < min) {
+				min = spectrum[s];
+			}
+		}
+
+		(*noiseFloor)[curBin] = min;
+		(*peakPower)[curBin] = max;
+		(*avgPower)[curBin] = avg / n;
+
+		curBin++;
+		startSample = endSample;
 	}
 }
-*/
+
+void updateLineFromAmplitudeVector(const SpectrumAccumulator::AmplitudeVector &vector,
+                                   const sf::Vector2u &windowSize,
+                                   sf::VertexArray *line)
+{
+	double h = windowSize.y;
+	double scale = h / 50;
+
+	for(size_t i = 0; i < vector.size(); i++) {
+		double absVal = vector[i];
+
+		double dB = 10 * std::log10(absVal);
+
+		(*line)[i].position.y = -dB * scale;
+
+		/*
+			 double alpha = (*line)[i].position.y / h;
+
+			 if     (alpha < 0) alpha = 0;
+			 else if(alpha > 1) alpha = 1;
+
+			 (*line)[i].color.r = botColor.r * alpha + topColor.r * (1 - alpha);
+			 (*line)[i].color.g = botColor.g * alpha + topColor.g * (1 - alpha);
+			 (*line)[i].color.b = botColor.b * alpha + topColor.b * (1 - alpha);
+			 */
+	}
+}
 
 int main(void)
 {
-	size_t npoints = 1;
-	SpectrumAccumulator::AmplitudeVector spectrum;
+	SpectrumAccumulator::AmplitudeVector spectrum, noiseFloor, avgPower, peakPower;
 
 	sf::RenderWindow window(sf::VideoMode(1024, 768), "OsmoSDR Spectrum Analyzer");
 	window.setFramerateLimit(30);
 
-	sf::VertexArray fftLine(sf::LinesStrip, npoints);
 	sf::VertexArray cursorLine(sf::Lines, 2);
 	cursorLine[0].color = sf::Color::White;
 	cursorLine[1].color = sf::Color::White;
+
+	sf::VertexArray noiseFloorLine(sf::LinesStrip, 1024);
+	sf::VertexArray avgPowerLine(sf::LinesStrip, 1024);
+	sf::VertexArray peakPowerLine(sf::LinesStrip, 1024);
 
 	sf::Font infoFont;
 	infoFont.loadFromFile("/usr/share/fonts/dejavu/DejaVuSans.ttf");
@@ -40,8 +95,9 @@ int main(void)
 	sf::Text infoText("InfoText", infoFont, 30);
 	infoText.setPosition(10, 10);
 
-	sf::Color botColor = sf::Color::Red;
-	sf::Color topColor = sf::Color::Green;
+	sf::Color noiseFloorColor = sf::Color::Blue;
+	sf::Color avgPowerColor   = sf::Color::Green;
+	sf::Color peakPowerColor  = sf::Color::Red;
 
 	Receiver receiver("hackrf=0", 4096, 12.5e6);
 
@@ -52,16 +108,7 @@ int main(void)
 	std::cout << "Hardware freq range: [" << receiver.getHardwareMinFreq() << ", " << receiver.getHardwareMaxFreq() << "]\n";
 	std::cout << "Sweep freq range: [" << receiver.getSweepMinFreq() << ", " << receiver.getSweepMaxFreq() << "]\n";
 
-	//receiver.setSweepFreqRange(50e6, 150e6);
-
-	receiver.getCurrentResults(&spectrum);
-	npoints = spectrum.size();
-	fftLine.resize(npoints);
-
-	for(size_t i = 0; i < npoints; i++) {
-		fftLine[i].position.x = (200 / npoints) * i + (200 / npoints / 2);
-		fftLine[i].position.y = 100;
-	}
+	receiver.setSweepFreqRange(2.4e9, 2.5e9);
 
 	while(window.isOpen())
 	{
@@ -104,39 +151,35 @@ int main(void)
 				cursorLine[0].position.y = 0;
 				cursorLine[1].position.y = window.getSize().y;
 
-				for(size_t i = 0; i < npoints; i++) {
-					double w = window.getSize().x;
+				size_t nbins = window.getSize().x;
 
-					fftLine[i].position.x = (w / npoints) * i + (w / npoints / 2);
+				noiseFloorLine.resize(nbins);
+				avgPowerLine.resize(nbins);
+				peakPowerLine.resize(nbins);
+
+				for(size_t i = 0; i < nbins; i++) {
+					noiseFloorLine[i].position.x = i;
+					avgPowerLine[i].position.x   = i;
+					peakPowerLine[i].position.x  = i;
+
+					noiseFloorLine[i].color      = noiseFloorColor;
+					avgPowerLine[i].color        = avgPowerColor;
+					peakPowerLine[i].color       = peakPowerColor;
 				}
 			}
 		}
 
-		double h = window.getSize().y;
-		double scale = h / 50;
-
 		receiver.getCurrentResults(&spectrum);
-
-		for(size_t i = 0; i < npoints; i++) {
-			double absVal = spectrum[i];
-
-			double dB = 10 * std::log10(absVal);
-
-			fftLine[i].position.y = -dB * scale;
-
-			double alpha = fftLine[i].position.y / h;
-
-			if     (alpha < 0) alpha = 0;
-			else if(alpha > 1) alpha = 1;
-
-			fftLine[i].color.r = botColor.r * alpha + topColor.r * (1 - alpha);
-			fftLine[i].color.g = botColor.g * alpha + topColor.g * (1 - alpha);
-			fftLine[i].color.b = botColor.b * alpha + topColor.b * (1 - alpha);
-		}
+		resultToScreenData(spectrum, window.getSize().x, &noiseFloor, &avgPower, &peakPower);
+		updateLineFromAmplitudeVector(noiseFloor, window.getSize(), &noiseFloorLine);
+		updateLineFromAmplitudeVector(avgPower,   window.getSize(), &avgPowerLine);
+		updateLineFromAmplitudeVector(peakPower,  window.getSize(), &peakPowerLine);
 
 		window.clear();
 		window.draw(cursorLine);
-		window.draw(fftLine);
+		window.draw(noiseFloorLine);
+		window.draw(avgPowerLine);
+		window.draw(peakPowerLine);
 		window.draw(infoText);
 		window.display();
 	}
